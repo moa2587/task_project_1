f7f16a5ccc0446bba9145697b3ddd2945605009c
moa2587 <moa25874389@gmail.com>
2024-12-29 10:45:58 +0330

    change_2

--- task1.py
+++ task1.py
@@ -28,7 +28,7 @@ def check_number_wall(x, table_wall):
 def do_something(x):
     print("1-> move \n2-> place wall \nchoose : ")
     s = input()
-    if(s == '1'): print(0)
+    if(s == '1'): print(2)
     else: print(1)
 
 do_something(1)
